9db93ac42b4c45155c2efbbe36d75129e6a2b61c
Dariaman <dariaman.siagian@gmail.com>
2025-10-12 16:58:20 +0700

    update default seed

--- SQLFile/DefaultTenantSeed.sql
+++ SQLFile/DefaultTenantSeed.sql
@@ -1,7 +1,8 @@
 
 SET IDENTITY_INSERT Tenant ON
 
-INSERT INTO dbo.Tenant(id,TenantName,CreatedBy)
-SELECT 1,'Internal',1
+INSERT INTO dbo.Tenant(Id,TenantName,IsActive,CreatedBy)
+SELECT 0,'NONE',0,1 union all
+SELECT 1,'Internal',1,1
 
 SET IDENTITY_INSERT Tenant OFF
